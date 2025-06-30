d8b32c4b18f86d24e5eb0fe5dd41ad49a1ddcaa2
SCHULE\krause_02 <markkrause@outlook.de>
2025-06-30 11:58:17 +0200

    removed unnecessary changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,12 +11,6 @@ public class HelloWorld {
 
         System.out.println("Hallo, " + name + "! Schön, dich kennenzulernen.");
 
-        // Bonus: einfache Interaktion mit Zahlen
-        System.out.print("Gib bitte eine Zahl ein: ");
-        int zahl = scanner.nextInt();
-
-        System.out.println("Die Zahl " + zahl + " mal 2 ergibt: " + (zahl * 2));
-
         // Scanner schließen (optional, aber gute Praxis)
         scanner.close();
     }
