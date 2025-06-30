97d89c52894c836599a3285c4042bdf2f7eda1d6
SCHULE\krause_02 <markkrause@outlook.de>
2025-06-30 11:56:03 +0200

    more complex interactive

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Aenderung1");
-        
+        // Scanner zum Einlesen von Benutzereingaben
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wie heißt du?");
+        System.out.print("Bitte gib deinen Namen ein: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hallo, " + name + "! Schön, dich kennenzulernen.");
+
+        // Bonus: einfache Interaktion mit Zahlen
+        System.out.print("Gib bitte eine Zahl ein: ");
+        int zahl = scanner.nextInt();
+
+        System.out.println("Die Zahl " + zahl + " mal 2 ergibt: " + (zahl * 2));
+
+        // Scanner schließen (optional, aber gute Praxis)
+        scanner.close();
     }
 }
